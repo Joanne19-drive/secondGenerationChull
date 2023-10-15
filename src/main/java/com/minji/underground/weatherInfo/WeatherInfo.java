218731a21cead61c84b26a8dbf381ebdfb079bed
고객데이터플랫폼팀 곽민지 <kwak.mj@sk.com>
2023-10-16 08:52:20 +0900

    timezone 수정

--- src/main/java/com/minji/underground/weatherInfo/WeatherInfo.java
+++ src/main/java/com/minji/underground/weatherInfo/WeatherInfo.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
@@ -19,9 +20,10 @@ public class WeatherInfo {
     OkHttpClient client = new OkHttpClient();
 
     public String currentWeather() throws IOException {
-        LocalDate now = LocalDate.now();
+        ZoneId koreaZone = ZoneId.of("Asia/Seoul");
+        ZonedDateTime koreaTime = ZonedDateTime.now(koreaZone);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        String formatedNow = now.format(formatter);
+        String formatedNow = koreaTime.format(formatter);
 
         String apiUrl = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidFcst?serviceKey=" + weatherAppKey +
                 "&pageNo=1&numOfRows=10&dataType=json&stnId=108&tmFc=" + formatedNow + "0600";
