b9a93028f30750d841846b75874fc6660246856c
고객데이터플랫폼팀 곽민지 <kwak.mj@sk.com>
2023-10-16 10:06:36 +0900

    add responses

--- src/main/java/com/minji/underground/slack/ResponseMap.java
+++ src/main/java/com/minji/underground/slack/ResponseMap.java
@@ -28,7 +28,7 @@ public class ResponseMap {
         inputToResponse.put("떠나요 둘이서~", "모든 걸 훌훌 버리고~");
         inputToResponse.put("윤회 불러줘", "<@U059GEF25DW>야 너를 찾는다");
         inputToResponse.put("민지 불러줘", "<@U0595CLN20Z>님 당신을 찾습니다");
-        inputToResponse.put("상화 불러줘", "<@U058V896M9A>야~? 어디니~?");
+        inputToResponse.put("상화 불러줘", "<@U058V896M9A>~? 어디니~?");
         inputToResponse.put("승준 불러줘", "<@U058V83P8LU> 말고 그냥 승준 나와");
         inputToResponse.put("현지 불러줘", "<@U0595CLKC73>인 듯 정상 아닌 정상 같은 분~");
         inputToResponse.put("2대철이 불러줘", "뭐;;");
